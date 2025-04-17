ade2fe729644bda3729d3b26fe7cd3ff75ad07ae
buraktnv <buraktnv@gmail.com>
2025-04-17 20:03:53 +0300

    key moved to .env

--- src/components/Recommender.vue
+++ src/components/Recommender.vue
@@ -27,10 +27,12 @@ import { ref, watch, onMounted, defineProps } from 'vue'
 import CardItem from './recommenderComponents/CardItem.vue';
 import { useFetch } from '../helpers/fetch';
 
+console.log(import.meta.env)
+
 const movies = ref([]);
 const currentSelection = ref('popular')
-const popularEndpoint = 'https://api.themoviedb.org/3/discover/movie?api_key=348088421ad3fb3a9d6e56bb6a9a8f80&include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc';
-const topRatedEndpoint = 'https://api.themoviedb.org/3/discover/movie?api_key=348088421ad3fb3a9d6e56bb6a9a8f80&include_adult=false&include_video=false&language=en-US&page=1&sort_by=vote_average.desc&without_genres=99,10755&vote_count.gte=200'
+const popularEndpoint = `https://api.themoviedb.org/3/discover/movie?api_key=${ import.meta.env.VITE_MY_API_KEY }&include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc'`
+const topRatedEndpoint = `https://api.themoviedb.org/3/discover/movie?api_key=${ import.meta.env.VITE_MY_API_KEY }&include_adult=false&include_video=false&language=en-US&page=1&sort_by=vote_average.desc&without_genres=99,10755&vote_count.gte=200`
 
 const fetchPopularMovies = async () => {
   movies.value = await useFetch(popularEndpoint);
